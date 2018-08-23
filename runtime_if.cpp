0d5ddde077a3016b4741cbae650b1b1e4fbc9c83
Snarpix <snarpix@gmail.com>
2018-08-23 19:38:43 +0300

    Added ability to enable RIF blocks. Removed idea of nopping - use branches. Removed unneeded static keywords

--- runtime_if.cpp
+++ runtime_if.cpp
@@ -27,8 +27,8 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-static const std::size_t PAGE_SIZE = getpagesize();
-static const std::uintptr_t PAGE_MASK = ~(static_cast<uintptr_t>(PAGE_SIZE) - 1);
+const std::size_t PAGE_SIZE = getpagesize();
+const std::uintptr_t PAGE_MASK = ~(static_cast<uintptr_t>(PAGE_SIZE) - 1);
 
 static
 void* page_align_ptr(void* ptr) {
@@ -47,65 +47,54 @@ void protect(void* ptr) {
     mprotect(ptr, PAGE_SIZE, PROT_READ | PROT_EXEC);
 }
 
-static constexpr std::uint8_t nops[9][9] = {
-    {0x90},
-    {0x66, 0x90},
-    {0x0F, 0x1F, 0x00},
-    {0x0F, 0x1F, 0x40, 0x00},
-    {0x0F, 0x1F, 0x44, 0x00, 0x00},
-    {0x66, 0x0F, 0x1F, 0x44, 0x00, 0x00},
-    {0x0F, 0x1F, 0x80, 0x00, 0x00, 0x00, 0x00},
-    {0x0F, 0x1F, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00},
-    {0x66, 0x0F, 0x1F, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00},
-};
-static constexpr std::size_t nop_max_size = sizeof(nops)/sizeof(nops[0]);
+constexpr std::uint8_t nop5[] = {0x0F, 0x1F, 0x44, 0x00, 0x00};
+constexpr std::uint8_t jmp_rel8 = 0xEB;
+constexpr std::size_t jmp_rel8_size = sizeof(jmp_rel8) + sizeof(int8_t);
+constexpr std::uint8_t jmp_rel32 = 0xE9;
+constexpr std::size_t jmp_rel32_size = sizeof(jmp_rel32) + sizeof(int32_t);
+constexpr std::uint8_t x86_trap = 0xCC; 
+static_assert(sizeof(nop5) == jmp_rel32_size);
 
-static constexpr std::uint8_t jmp_rel8 = 0xEB;
-static constexpr std::uint8_t jmp_rel8_size = sizeof(jmp_rel8) + sizeof(int8_t);
-static constexpr std::uint8_t jmp_rel32 = 0xE9;
-static constexpr std::uint8_t jmp_rel32_size = sizeof(jmp_rel32) + sizeof(int32_t);
-static constexpr std::size_t jmp_optimization_threshold = 16;
-
-void nopify(std::uint8_t* begin, std::uint8_t* end) {
+static 
+std::size_t get_rif_size(std::uint8_t* begin, std::uint8_t* end) {
     if(end <= begin) {
-        std::cout << "Invalid size of nops" << std::endl;
+        std::cout << "Invalid begin and end for region" << std::endl;
         abort();
     }
     std::size_t size = end - begin;
     if(size > PAGE_SIZE) {
-        std::cout << "Nop region too big. Implement >2 page support?" << std::endl;
+        std::cout << "RIF block too big. Implement >2 page support?" << std::endl;
         abort();
     }
+    return size;
+}
 
-    auto begin_page = page_align_ptr(begin);
-    auto end_page = page_align_ptr(end - 1);
+static 
+void generate_rif_jump(std::uint8_t (&jmp_buffer)[sizeof(nop5)], std::size_t size) {
+    if(size <= std::numeric_limits<int8_t>::max()) {
+        jmp_buffer[0] = jmp_rel8;
+        int8_t jmp_offset = size - jmp_rel8_size;
+        memcpy(&jmp_buffer[1], &jmp_offset, sizeof(jmp_offset));
+        memset(jmp_buffer + jmp_rel8_size, x86_trap, sizeof(nop5) - jmp_rel8_size);
+    } else if(size <= std::numeric_limits<int32_t>::max()) {
+        jmp_buffer[0] = jmp_rel32;
+        int32_t jmp_offset = size - jmp_rel32_size;
+        memcpy(&jmp_buffer[1], &jmp_offset, sizeof(jmp_offset));
+    } else {
+        std::cout << "Region too big event for 32 bit branch?!" << std::endl;
+        abort();
+    }
+}
+
+void write_code(std::uint8_t* begin, std::uint8_t* end, const std::uint8_t (&buf)[sizeof(nop5)]) {
+    const auto begin_page = page_align_ptr(begin);
+    const auto end_page = page_align_ptr(end - 1);
     unprotect(begin_page);
     if(begin_page != end_page) {
         unprotect(end_page);
     }
 
-    if(size >= jmp_optimization_threshold) {
-        if(size <= std::numeric_limits<int8_t>::max()) {
-            *begin++ = jmp_rel8;
-            int8_t jmp_offset = size - jmp_rel8_size;
-            memcpy(begin, &jmp_offset, sizeof(jmp_offset));
-            size -= jmp_rel8_size;
-            begin += sizeof(jmp_offset);
-        } else if(size <= std::numeric_limits<int32_t>::max()) {
-            *begin++ = jmp_rel32;
-            int32_t jmp_offset = size - jmp_rel32_size;
-            memcpy(begin, &jmp_offset, sizeof(jmp_offset));
-            size -= jmp_rel32_size;
-            begin += sizeof(jmp_offset);
-        }
-    }
-
-    while(size) {
-        std::size_t nop_size = size >= nop_max_size ? nop_max_size : size;
-        memcpy(begin, nops[nop_size - 1], nop_size);
-        size -= nop_size;
-        begin += nop_size;
-    }
+    memcpy(begin, buf, sizeof(nop5));
 
     protect(begin_page);
     if(begin_page != end_page) {
@@ -113,8 +102,47 @@ void nopify(std::uint8_t* begin, std::uint8_t* end) {
     }
 }
 
+void rif_disable(std::uint8_t* begin, std::uint8_t* end) {
+    std::size_t size = get_rif_size(begin, end);
+
+    std::uint8_t jmp_buffer[sizeof(nop5)];
+    generate_rif_jump(jmp_buffer, size);
+
+    if(memcmp(begin, nop5, sizeof(nop5)) != 0) {
+        if(memcmp(begin, jmp_buffer, sizeof(jmp_buffer)) == 0) {
+            std::cout << "Trying to disable already disabled RIF block" << std::endl;
+        } else {
+            std::cout << "Try to disable something that is not a RIF block" << std::endl;
+            abort();
+        }
+    }
+
+    write_code(begin, end, jmp_buffer);
+}
+
+void rif_enable(std::uint8_t* begin, std::uint8_t* end) {
+    std::size_t size = get_rif_size(begin, end);
+
+    std::uint8_t jmp_buffer[sizeof(nop5)];
+    generate_rif_jump(jmp_buffer, size);
+
+    if(memcmp(begin, jmp_buffer, sizeof(jmp_buffer)) != 0) {
+        if(memcmp(begin, nop5, sizeof(nop5)) == 0) {
+            std::cout << "Trying to enable already enable RIF block" << std::endl;
+        } else {
+            std::cout << "Try to enable something that is not a RIF block" << std::endl;
+            abort();
+        }
+    }
+
+    write_code(begin, end, nop5);
+}
+
 #define RUNTIME_IF_BLOCK(func, code) \
-    asm volatile ("_" #func "_rif_start:"); \
+    asm volatile ( \
+        "_" #func "_rif_start:\n\t" \
+        ".byte 0x0F, 0x1F, 0x44, 0x00, 0x00" \
+        ); \
     { \
         code \
     } \
@@ -124,12 +152,18 @@ void nopify(std::uint8_t* begin, std::uint8_t* end) {
     do { \
         extern std::uint8_t func ## _rif_start; \
         extern std::uint8_t func ## _rif_end; \
-        nopify(&func ## _rif_start, &func ## _rif_end); \
+        rif_disable(&func ## _rif_start, &func ## _rif_end); \
+    } while(0)
+
+#define RUNTIME_IF_ENABLE(func) \
+    do { \
+        extern std::uint8_t func ## _rif_start; \
+        extern std::uint8_t func ## _rif_end; \
+        rif_enable(&func ## _rif_start, &func ## _rif_end); \
     } while(0)
 
 // USAGE:
 
-// Both should be noinline!
 void debug1() {
     std::cout << "DEBUG: 1111!!!" << std::endl;
 }
@@ -169,6 +203,18 @@ int main() {
     foo();
     std::cout << "Disable 3!" << std::endl;
     RUNTIME_IF_DISABLE(debug3_rif);
+    std::cout << "Test Debug!" << std::endl;
+    foo();
+    std::cout << "Enable 2!" << std::endl;
+    RUNTIME_IF_ENABLE(debug2_rif);
+    std::cout << "Test Debug!" << std::endl;
+    foo();
+    std::cout << "Enable 3!" << std::endl;
+    RUNTIME_IF_ENABLE(debug3_rif);
+    std::cout << "Test Debug!" << std::endl;
+    foo();
+    std::cout << "Enable 1!" << std::endl;
+    RUNTIME_IF_ENABLE(debug1_rif);
     std::cout << "Test Debug!" << std::endl;
     foo();
     return 0;
